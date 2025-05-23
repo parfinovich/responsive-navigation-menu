1ff10df415c3b1c1dbfeeb866dd6f5a911a3c294
Arsen Parfinovich <parfinovich@icloud.com>
2025-05-23 10:26:35 +0300

    First commit for menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const burger = document.getElementById('burger');
+const navMenu = document.getElementById('nav-menu');
+
+burger.addEventListener('click', () => {
+	navMenu.classList.toggle('active');
+});
